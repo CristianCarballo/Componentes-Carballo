0af207388641db568b0a3312aad0d417ee5b9643
Cristian Carballo <cristian.carballo1994@gmail.com>
2022-11-23 20:34:56 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppToolbarComponent } from './toolbar/app-toolbar/app-toolbar.component';
+import { AppNavbarComponent } from './navbar/app-navbar/app-navbar.component';
+import { AppContentComponent } from './content/app-content/app-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolbarComponent,
+    AppNavbarComponent,
+    AppContentComponent
   ],
   imports: [
     BrowserModule
